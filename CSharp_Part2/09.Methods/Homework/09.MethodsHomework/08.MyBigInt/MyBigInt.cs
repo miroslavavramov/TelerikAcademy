dce3c568de9fe8f06ea7e0e30f1b2f68427b613f
stinger907 <stinger907@yahoo.com>
2013-12-21 18:21:34 +0200

    Reshuffle

---
+++ CSharp_Part2/09.Methods/Homework/09.MethodsHomework/08.MyBigInt/MyBigInt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Write a method that adds two positive integer numbers represented as arrays of digits
+//(each array element arr[i] contains a digit; the last digit is kept in arr[0]). 
+//Each of the numbers that will be added could have up to 10 000 digits.
+
+class MyBigInt
+{
+    static void Main()
+    {
+        PrintTrangle(5);
+    }
+    static void PrintTrangle(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
