493a66164d22b012e704789fefc437fa72b45a4d
stinger907 <stinger907@yahoo.com>
2013-12-21 23:22:32 +0200

    New Problem Solved

--- CSharp_Part2/09.Methods/Homework/09.MethodsHomework/08.MyBigInt/MyBigInt.cs
+++ CSharp_Part2/09.Methods/Homework/09.MethodsHomework/08.MyBigInt/MyBigInt.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 //Write a method that adds two positive integer numbers represented as arrays of digits
 //(each array element arr[i] contains a digit; the last digit is kept in arr[0]). 
 //Each of the numbers that will be added could have up to 10 000 digits.
@@ -7,17 +9,64 @@ class MyBigInt
 {
     static void Main()
     {
-        PrintTrangle(5);
+        DateTime start = DateTime.Now;
+
+        //string str1 = new string('9', (int)1E+4);         //Performance Test
+        //string str2 = new string('9', (int)1E+4);
+
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+
+        if (str1.Length > str2.Length)
+            str2 = new string('0', str1.Length - str2.Length) + str2;
+        else
+            str1 = new string('0', str2.Length - str1.Length) + str1;
+
+        StringBuilder sb1 = new StringBuilder(); sb1.Append(Reverse(str1));
+        StringBuilder sb2 = new StringBuilder(); sb2.Append(Reverse(str2));
+        Console.WriteLine(Reverse(Add(sb1, sb2).ToString()));
+        DateTime end = DateTime.Now;
+        Console.WriteLine("Execution Time: " + (end - start));
     }
-    static void PrintTrangle(int n)
+    static StringBuilder Add(StringBuilder s1, StringBuilder s2)
     {
-        for (int i = 1; i <= n; i++)
+        StringBuilder result = new StringBuilder();
+        int temp = 0 ;
+
+        for (int i = 0; i < s1.Length; i++)
         {
-            for (int k = 1; k <= i; k++)
+            if (s1[i] >= 48 && s1[i] <= 57 && s2[i] >= 48 && s2[i] <= 57)   
             {
-                Console.Write(k + " ");
+                temp += (int)(char.GetNumericValue(s1[i]) + char.GetNumericValue(s2[i]));
+                if (temp > 9 && i == s1.Length - 1)
+                {
+                    result.Append( temp % 10);
+                    result.Append( temp / 10);
+                }
+                else if (temp > 9)
+                {
+                    result.Append(temp - 10);
+                    temp = 1;
+                }
+                else
+                {
+                    result.Append(temp);
+                    temp = 0;
+                }
             }
-            Console.WriteLine();
+            else
+            {
+                Console.WriteLine("Invalid Input!");
+                return result.Clear();
+            }   
         }
+        return result;
     }
+    static string Reverse(string s)
+    {
+        char[] arr = s.ToCharArray();        
+        Array.Reverse(arr);
+        return new string (arr);
+    }
+   
 }
