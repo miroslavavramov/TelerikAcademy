d7e61a82697c87387529578721a6c112adfd43d6
stinger907 <stinger907@yahoo.com>
2013-12-23 10:05:28 +0200

    update task 7

--- CSharp_Part2/09.Methods/Homework/09.MethodsHomework/08.MyBigInt/MyBigInt.cs
+++ CSharp_Part2/09.Methods/Homework/09.MethodsHomework/08.MyBigInt/MyBigInt.cs
@@ -17,33 +17,33 @@ class MyBigInt
         string str1 = Console.ReadLine();
         string str2 = Console.ReadLine();
 
-        if (str1.Length > str2.Length)
-            str2 = str2.Insert(0, new string('0', str1.Length - str2.Length));
-        else if (str1.Length < str2.Length)
-            str1 = str1.Insert(0, new string('0', str2.Length - str1.Length));
-
-        StringBuilder sb1 = new StringBuilder(); sb1.Append(Reverse(str1));
-        StringBuilder sb2 = new StringBuilder(); sb2.Append(Reverse(str2));
-        
-        Console.WriteLine(Reverse(Add(sb1, sb2).ToString()));
+        Console.WriteLine(Add(str1, str2));
 
         //DateTime end = DateTime.Now;
         //Console.WriteLine("Execution Time: " + (end - start));
     }
-    static StringBuilder Add(StringBuilder s1, StringBuilder s2)
+    static string Add(string s1, string s2)
     {
+        if (s1.Length > s2.Length)
+            s2 = s2.Insert(0, new string('0', s1.Length - s2.Length));
+        else if (s1.Length < s2.Length)
+            s1 = s1.Insert(0, new string('0', s2.Length - s1.Length));
+
+        s1 = Reverse(s1);
+        s2 = Reverse(s2);
+
         StringBuilder result = new StringBuilder();
-        int temp = 0 ;
+        int temp = 0;
 
         for (int i = 0; i < s1.Length; i++)
         {
-            if (s1[i] >= 48 && s1[i] <= 57 && s2[i] >= 48 && s2[i] <= 57)   
+            if (s1[i] >= 48 && s1[i] <= 57 && s2[i] >= 48 && s2[i] <= 57)
             {
                 temp += (int)(char.GetNumericValue(s1[i]) + char.GetNumericValue(s2[i]));
                 if (temp > 9 && i == s1.Length - 1)
                 {
-                    result.Append( temp % 10);
-                    result.Append( temp / 10);
+                    result.Append(temp % 10);
+                    result.Append(temp / 10);
                 }
                 else if (temp > 9)
                 {
@@ -59,10 +59,10 @@ class MyBigInt
             else
             {
                 Console.WriteLine("Invalid Input!");
-                return result.Clear();
-            }   
+                return result.Clear().ToString();
+            }
         }
-        return result;
+        return Reverse(result.ToString());
     }
     static string Reverse(string s)
     {
