30ccbe1b8cd977703c2f90d0bf48551909a7a455
stinger907 <stinger907@yahoo.com>
2014-01-01 00:52:19 +0200

    Classes and Objects HW added

---
+++ CSharp_Part2/11.ClassesAndObjects/Homework/11.ClassesAndObjectsHW/02.RandomValues/RandomValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Write a program that generates and prints to the console 10 random values in the range [100, 200].
+class RandomValues
+{
+    static readonly Random globalRandomGenerator = new Random();
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(globalRandomGenerator.Next(100, 201));
+        }
+    }
+}
+
