c83b4daba27dcd8d69f7e03994e1a79953c747b0
stinger907 <stinger907@yahoo.com>
2014-01-08 11:38:26 +0200

    14.Strings And Text Processing HW added

---
+++ CSharp_Part2/14.StringsAndTextProcessing/Homework/14.StringsAndTextProcessingHW/11.FormatNumber/FormatNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//Write a program that reads a number and prints it as a decimal number, hexadecimal number, 
+//percentage and in scientific notation. Format the output aligned right in 15 symbols.
+class FormatNumber
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0,15:D}", num);
+        Console.WriteLine("{0,15:X}", num);
+        Console.WriteLine("{0,15:P}", num);
+        Console.WriteLine("{0,15:E}", num);
+    }
+}
+
---
+++ CSharp_Part2/14.StringsAndTextProcessing/Homework/14.StringsAndTextProcessingHW/20.ExtractPalindromes/ExtractPalindomes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+//Write a program that extracts from a given text all palindromes, e.g. "ABBA", "lamal", "exe".
+class ExtractPalindomes
+{
+    static void Main()
+    {
+        string text = @"Some people have names that are palindromes. Included are given names 
+                        (Ada, Anna, Bob, Aviva), surnames (Harrah, Renner, Salas, Arora) 
+                        or both (Eve, Hannah, Maham, Otto)";
+
+        var words = Regex.Matches(text, @"\b\w+\b");
+
+        foreach (var word in words)
+            if (IsPalindrome(word.ToString().ToUpper())) Console.WriteLine(word);
+        
+    }
+    static bool IsPalindrome(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+            if (s[i] != s[s.Length - 1 - i]) return false;
+
+        return true;
+    }
+}
