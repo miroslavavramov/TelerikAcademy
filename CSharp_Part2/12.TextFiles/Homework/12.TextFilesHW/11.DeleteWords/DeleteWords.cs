3c47d112f41bdfdf3df70aa060cb6865d3ecefe8
stinger907 <stinger907@yahoo.com>
2014-01-08 12:19:37 +0200

    updated Task 10 & 11

--- CSharp_Part2/12.TextFiles/Homework/12.TextFilesHW/11.DeleteWords/DeleteWords.cs
+++ CSharp_Part2/12.TextFiles/Homework/12.TextFilesHW/11.DeleteWords/DeleteWords.cs
@@ -12,7 +12,7 @@ class DeleteWords
             using (StreamWriter writer = new StreamWriter(@"..\..\output.txt"))
             {
                 string text = reader.ReadToEnd();
-                writer.WriteLine(Regex.Replace(text, @"\b(test)\w+", ""));
+                writer.WriteLine(Regex.Replace(text, @"\b(test)\w*", ""));
             }
         }
     }
