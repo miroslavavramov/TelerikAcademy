0c3eeb008f530f075a04900ab6b24fde860b12a4
stinger907 <stinger907@yahoo.com>
2014-02-04 19:35:39 +0200

    People.csproj added

---
+++ CSharp_OOP/18.OOP_Principles_I/People/People.Common/Human.cs
@@ -0,0 +1,16 @@
+﻿namespace People.Common
+{
+    using System;
+
+    public abstract class Human
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Human(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
